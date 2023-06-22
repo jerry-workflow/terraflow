0b4a38c2e92f8fdb3249752b991f04fbd47a41cc
Jerry Man <jerryman@MacBook-Pro.local>
2023-06-22 09:04:03 -0400

    updated plan.yml

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.my-region
-  access_key = var.my-access-key
-  secret_key = var.my-secret-key
+  # access_key = var.my-access-key
+  # secret_key = var.my-secret-key
 }
