966d3cbf4a459cdd034710bb4ea560a4a385e595
Jerry Man <jerryman@MacBook-Pro.local>
2023-06-22 09:24:56 -0400

    updated plan.yml

--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "my-region" {
   type        = string 
   default = "us-east-1"
 }
-# variable "my-access-key" {
-#   description = "this is access key"
-#   type        = string 
-#   default = "AKIAXF4BLG3W5EI32EGD"
-# }
+variable "my-access-key" {
+  description = "this is access key"
+  type        = string 
+  default = "AKIAXF4BLG3W5EI32EGD"
+}
 
-# variable "my-secret-key" {
-#   description = "this is secret key"
-#   type        = string 
-#   default = "gFOGkvCu/qm04+XhVxulBGPNFFc+8OyjS69/1B8a"
-# }
+variable "my-secret-key" {
+  description = "this is secret key"
+  type        = string 
+  default = "gFOGkvCu/qm04+XhVxulBGPNFFc+8OyjS69/1B8a"
+}
